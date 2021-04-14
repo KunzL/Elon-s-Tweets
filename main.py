fe2acec0acb864b59c2047a4b6ced95f05f24ab9
Luc Kunz <luc.kunz@pm.me>
2021-04-14 13:18:23 +0200

    import yahoo data

--- main.py
+++ main.py
@@ -1,1 +1,1 @@
-# Resume Project
+#%% Resume Project 1import pandas as pdimport yfinance as yffrom yahoofinancials import YahooFinancialsimport datetimefrom dateutil.relativedelta import relativedelta#%% Import Datanow = datetime.datetime.now()start = now + relativedelta(years=-2) + relativedelta(days=1)data = yf.download('SPY ',                   interval='1h',                   start=start,                    #end=,                   auto_adjust = True,                   progress=True)#%%if __name__ == "__main__":   print("im a bg")
